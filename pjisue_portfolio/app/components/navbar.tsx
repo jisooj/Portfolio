8c04d58bcb470923b4a03c1160994924b86b0789
jisooj <jisooj0403@gmail.com>
2025-07-17 01:33:40 -0700

    style fix

--- pjisue_portfolio/app/components/navbar.tsx
+++ pjisue_portfolio/app/components/navbar.tsx
@@ -40,11 +40,11 @@ export function NavBar() {
         </button>
 
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
             {
               PageInfo.map(info => (
-                <li>
-                  <NavLink key={`nav_${info.name}`}
+                <li key={`nav_${info.name}`}>
+                  <NavLink
                     to={`/${info.path}`}
                     className={({ isActive, isPending }) => {
                       // TODO: 
